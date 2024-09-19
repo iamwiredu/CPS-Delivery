67079e54bf3031271c58877e19c8f94d53a3c8b3
Kwaku Appenteng Wiredu <kwakuwiredu0@gmail.com>
2024-09-18 22:16:04 -0700

    current state

--- delivery/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import DeliveryRequest, Profile, ShopItem
-# Register your models here.
-
-
-admin.site.register(DeliveryRequest)
-admin.site.register(Profile)
-admin.site.register(ShopItem)
--- delivery/templatetags/delivery_extras.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-
-def split(arg1,arg2):
-    return arg1[0:arg2]
