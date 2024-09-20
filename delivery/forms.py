c7618486a80dadcc982982331392f9d022c2e221
Kwaku Appenteng Wiredu <kwakuwiredu0@gmail.com>
2024-09-20 00:11:52 -0700

    request page minor upgrades

--- delivery/forms.py
+++ delivery/forms.py
@@ -7,4 +7,13 @@ from .models import DeliveryRequest
 class DeliveryRequestForm(ModelForm):
     class Meta:
         model = DeliveryRequest
+        labels = {
+            "orderQuantity":"Quantity",
+            "pickupNumber":"Pickup Number",
+            "pickupPoint":"Pickup Point",
+            "dropoffNumber":"Dropoff Number",
+            "deliveryPoint":"Dropoff Point",
+            "productFee":"Product Fee",
+            "additionalInfo":"Additional Info",
+        }
         fields = ['orderQuantity','pickupNumber','pickupPoint','dropoffNumber','deliveryPoint','productFee','additionalInfo']
